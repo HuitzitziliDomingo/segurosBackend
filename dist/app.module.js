ec7a0cd8ad643fba718e7aeed8f7adf450411f5f
Leon <huitzitzilidomingo@gmail.com>
2026-04-03 00:09:29 -0600

    FEAT new configurations app

--- dist/app.module.js
+++ dist/app.module.js
@@ -30,7 +30,7 @@ exports.AppModule = AppModule = __decorate([
                 port: parseInt(process.env.DB_PORT || '5432'),
                 username: process.env.DB_USER || 'postgres',
                 password: process.env.DB_PASSWORD || 'postgres',
-                database: process.env.DB_NAME || 'hospital_db',
+                database: process.env.DB_NAME || 'seguros_db',
                 autoLoadEntities: true,
                 synchronize: process.env.NODE_ENV !== 'production',
                 logging: process.env.NODE_ENV !== 'production',
